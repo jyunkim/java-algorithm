1984012778bd5d9ac4b2a0786c205437cd60a340
JyunKim <kann1997@naver.com>
2021-07-07 00:48:14 +0900

    Initial commit

---
+++ src/baekjoon/NQueen_9663.java
@@ -0,0 +1,64 @@
+package baekjoon;
+
+import java.io.*;
+
+public class NQueen_9663 {
+
+    static int n;
+    static int[][] board;
+    static int answer = 0;
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        n = Integer.parseInt(br.readLine());
+        board = new int[n][n];
+        dfs(0);
+        System.out.println(answer);
+    }
+
+    public static void dfs(int line) {
+
+        // 종료 조건
+        if (line == n) {
+            answer++;
+            return;
+        }
+        for (int j = 0; j < n; j++) {
+            if (check(line, j)) {
+                board[line][j] = 1;
+                dfs(line + 1);
+                board[line][j] = 0;
+            }
+        }
+    }
+
+    public static boolean check(int row, int col) {
+
+        // 좌우
+        for (int j = 0; j < n; j++) {
+            if (board[row][j] != 0) {
+                return false;
+            }
+        }
+        // 상
+        for (int i = 0; i < row; i++) {
+            if (board[i][col] != 0) {
+                return false;
+            }
+        }
+        // 대각
+        for (int i = row, j = col; i >= 0 && j >= 0; i--, j--) {
+            if (board[i][j] != 0) {
+                return false;
+            }
+        }
+        // 대각
+        for (int i = row, j = col; i >= 0 && j < n; i--, j++) {
+            if (board[i][j] != 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
---
+++ src/baekjoon/Teach_1062.java
@@ -0,0 +1,74 @@
+package baekjoon;
+
+import java.io.*;
+import java.util.*;
+
+public class Teach_1062 {
+    static int n, k;
+    static ArrayList<String> words;
+    static int answer = 0;
+    static boolean[] learned;
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] input = br.readLine().split(" ");
+        n = Integer.parseInt(input[0]);
+        k = Integer.parseInt(input[1]);
+
+        String str;
+        words = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            str = br.readLine();
+            words.add(str.substring(4, str.length() - 4));
+        }
+
+        // 학습한 단어 체크
+        learned = new boolean[26];
+        learned['a' - 'a'] = true;
+        learned['c' - 'a'] = true;
+        learned['i' - 'a'] = true;
+        learned['n' - 'a'] = true;
+        learned['t' - 'a'] = true;
+
+        if (k < 5) {
+            System.out.println(0);
+        } else if (k == 26) {
+            System.out.println(n);
+        } else {
+            check(0, 0);
+            System.out.println(answer);
+        }
+    }
+
+    public static void check(int num, int index) {
+
+        // 종료 조건
+        // 다 배웠으면 개수 확인
+        if (num == k - 5) {
+            int count = 0;
+            for (String word : words) {
+                boolean ok = true;
+                for (int i = 0; i < word.length(); i++) {
+                    if (!learned[word.charAt(i) - 'a']) {
+                        ok = false;
+                        break;
+                    }
+                }
+                if (ok) {
+                    count++;
+                }
+            }
+            answer = Math.max(answer, count);
+            return;
+        }
+        // 학습
+        for (int i = index; i < learned.length; i++) {
+            if (!learned[i]) {
+                learned[i] = true;
+                check(num + 1, i + 1);
+                learned[i] = false;
+            }
+        }
+    }
+}
